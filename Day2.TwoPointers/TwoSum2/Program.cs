7ccb991e70b1439418f9aef0f39e33f73c2aefbb
Lyubomir Zaykov <lyubomirz@students.softuni.bg>
2022-12-11 10:08:56 +0200

    Solution for the TwoSum II problem and folders rearrangment

---
+++ Day2.TwoPointers/TwoSum2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+int target = int.Parse(Console.ReadLine());
+int[] indices = TwoSum(numbers, target);
+Console.WriteLine(String.Join(",", indices));
+int[] TwoSum(int[] numbers, int target)
+{
+    Dictionary<int, int> required = new Dictionary<int, int>();
+    int[] indices = new int[2];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        int currNumber = numbers[i];
+        if (required.ContainsKey(currNumber))
+        {
+            indices[0] = (required.GetValueOrDefault(currNumber)) + 1;
+            indices[1] = i + 1;
+            return indices;
+        }
+        int requiredNumber = target - currNumber;
+        if (required.ContainsKey(requiredNumber))
+        {
+            continue;
+        }
+        required.Add(requiredNumber, i);
+    }
+    return indices;
+
+}
