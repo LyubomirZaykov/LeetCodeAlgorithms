47e38b3f01c32645edd6e17051c59c311a62ddc5
Lyubomir Zaykov <lyubomirz@students.softuni.bg>
2022-12-14 23:47:29 +0200

    Revese string and Reverse words in string

---
+++ Day2.TwoPointers/ReverseString/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+char[] charArr = Console.ReadLine().Split(",").Select(char.Parse).ToArray();
+ReverseString(charArr);
+Console.WriteLine(string.Join(",", charArr));
+Console.ReadLine();
+void ReverseString(char[] s)
+{
+    StringBuilder sb = new StringBuilder();
+    for (int j = s.Length-1; j > -1; j--)
+    {
+        sb.Append(s[j]);
+    }
+    for (int i = 0; i < sb.Length; i++)
+    {
+        s[i] = sb[i];
+    }
+}
---
+++ Day2.TwoPointers/ReverseWordsInStringIII/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+string input = Console.ReadLine();
+string str=ReverseWords(input);
+Console.WriteLine(str);
+string ReverseWords(string s)
+{
+    string[] sArr = s.Split(" ");
+    for (int i = 0; i < sArr.Length; i++)
+    {
+        string currString= sArr[i];
+        StringBuilder sb=new StringBuilder();
+        for (int j = currString.Length-1; j >-1; j--)
+        {
+            sb.Append(currString[j]);
+        }
+        sArr[i] = sb.ToString();
+    }
+    return String.Join(" ",sArr);
+}
